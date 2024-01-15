bb9886216b06d85c4d0af11f909596cb85bb7c46
Eegleeyes <114844231+Eegleeyes@users.noreply.github.com>
2024-01-14 21:18:05 -0500

    Update ColorE_Source_Code.lua
    Added Meta methods (color3 * color3 and all other math operations work now without calling respective functions), Improved constants (colorE constant functions depreciated, replaced with just the name of the color, for example colorE.black instead of colorE.black().color), Added fromGreyScale (takes in one value and returns color with all channels being that value), added random color (colorE.random(), couldn't do the thing like the rest of the constants)

--- ColorE_Source_Code.lua
+++ ColorE_Source_Code.lua
@@ -28,7 +28,7 @@ Color3 Extended
 
 colorE.__index = colorE
 
-function colorE.new(R:number | Color3 | Vector3,G:number?,B:number?)
+function colorE.new(R:number,G:number,B:number)
 	local color = {}
 	setmetatable(color, colorE)
 	
@@ -54,7 +54,7 @@ function colorE.new(R:number | Color3 | Vector3,G:number?,B:number?)
 	return color
 end
 
-function colorE.fromHSV(Hue:number | Color3 | Vector3,Saturation:number?,Value:number?)
+function colorE.fromHSV(Hue:number,Saturation:number,Value:number)
 	local color = {}
 	setmetatable(color, colorE)
 
@@ -77,25 +77,21 @@ function colorE.fromHSV(Hue:number | Color3 | Vector3,Saturation:number?,Value:n
 	return color
 end
 
-function colorE.fromRGB(R:number | Color3 | Vector3,G:number?,B:number?) -- Basically the same as colorE.new()
+function colorE.fromRGB(R:number,G:number,B:number) -- Basically the same as colorE.new()
 	local color = {}
 	setmetatable(color, colorE)
 
 	if typeof(R) == "Color3" then --Detect if first value is a number, if not, assume it's a Color3
-		color.R = R.R
-		color.G = G.G
-		color.B = B.B
+		color.R = R.R/255
+		color.G = G.G/255
+		color.B = B.B/255
 		color.color = R
 	elseif typeof(R) == "number" then
 		color.R = R/255
 		color.G = G/255
 		color.B = B/255
 		color.color = Color3.fromRGB(R,G,B)
-	elseif typeof(R) == "Vector3" then
-		color.R = R.X/255
-		color.G = R.Y/255
-		color.B = R.Z/255
-		color.color = Color3.fromRGB(R.X,R.Y,R.Z)
+		
 	else
 		return error("Color input is not valid!") -- Throw an error if it isn't a color
 	end
@@ -103,7 +99,7 @@ function colorE.fromRGB(R:number | Color3 | Vector3,G:number?,B:number?) -- Basi
 	return color
 end
 
-function colorE.fromHex(Hex:string | Color3) -- Basically the same as colorE.new()
+function colorE.fromHex(Hex:string) -- Basically the same as colorE.new()
 	local color = {}
 	setmetatable(color, colorE)
 
@@ -119,6 +115,27 @@ function colorE.fromHex(Hex:string | Color3) -- Basically the same as colorE.new
 	return color
 end
 
+function colorE.fromGreyscale(x:number)
+	local color = {}
+	setmetatable(color, colorE)
+	color.R = x
+	color.G = x
+	color.B = x
+	color.color = Color3.new(x,x,x)
+
+	return color
+end
+
+colorE.black = Color3.new(0,0,0)
+colorE.white = Color3.new(1,1,1)
+colorE.red = Color3.new(1,0,0)
+colorE.green = Color3.new(0,1,0)
+colorE.blue = Color3.new(0,0,1)
+colorE.yellow = Color3.new(1,1,0)
+colorE.cyan = Color3.new(0,1,1)
+colorE.magenta = Color3.new(1,0,1)
+
+--[[
 function colorE.black()
 	local color = {}
 	setmetatable(color, colorE)
@@ -222,21 +239,17 @@ function colorE.magenta()
 
 	return color
 end
+]]
 
 function colorE.random()
-	local color = {}
-	setmetatable(color, colorE)
+	local R = math.random(0,10000)/10000
+	local G = math.random(0,10000)/10000
+	local B = math.random(0,10000)/10000
 
-	color.R = math.random(0,10000)/10000
-	color.G = math.random(0,10000)/10000
-	color.B = math.random(0,10000)/10000
-
-	color.color = Color3.new(color.R,color.G,color.B)
-
-	return color
+	return Color3.new(R,G,B)
 end
 
-function colorE:Lerp(Color:Color3 | 'ColorE', Alpha:number)
+function colorE:Lerp(Color, Alpha:number)
 	local color
 	if typeof(Color) == "Color3" then
 		color = self.color:Lerp(Color, Alpha)
@@ -245,7 +258,7 @@ function colorE:Lerp(Color:Color3 | 'ColorE', Alpha:number)
 	else
 		return error("Color input is not valid!")
 	end
-	return colorE.new(color)
+	return color
 end
 
 function colorE:ToHSV()
@@ -262,32 +275,26 @@ end
 
 function colorE:getLuminance1()
 	local L = 0
-	local R = self.color.R
-	local G = self.color.G
-	local B = self.color.B
+	local R,G,B = self:mult(1)
 	L = (0.2126*R + 0.7152*G + 0.0722*B) -- majik numbers
 	return L
 end
 
 function colorE:getLuminance2()
 	local L = 0
-	local R = self.color.R
-	local G = self.color.G
-	local B = self.color.B
+	local R,G,B = self:mult(1)
 	L = (0.299*R + 0.587*G + 0.114*B) -- even more majik numbers
 	return L
 end
 
 function colorE:getLuminance3()
 	local L = 0
-	local R = self.color.R
-	local G = self.color.G
-	local B = self.color.B
+	local R,G,B = self:mult(1)
 	L = math.sqrt(0.299*R^2 + 0.587*G^2 + 0.114*B^2) -- MORE majik numbers
 	return L
 end
 
-function colorE:mult(x:Color3 | Vector3 | 'ColorE')
+function colorE:mult(x:Color3 | Vector3 | ColorE)
 	local R,G,B
 	local t = typeof(x)
 	if t == "Color3" then
@@ -312,7 +319,32 @@ function colorE:mult(x:Color3 | Vector3 | 'ColorE')
 	return colorE.new(R,G,B)
 end
 
-function colorE:div(x:Color3 | Vector3 | 'ColorE')
+function colorE:__mul(x:Color3 | Vector3 | ColorE)
+	local R,G,B
+	local t = typeof(x)
+	if t == "Color3" then
+		R = self.R * x.R
+		G = self.G * x.G
+		B = self.B * x.B
+	elseif t == "Vector3" then
+		R = self.R * x.X
+		G = self.G * x.Y
+		B = self.B * x.Z
+	elseif t == "number" then
+		R = self.R * x
+		G = self.G * x
+		B = self.B * x
+	elseif t == "table" then
+		R = self.R * x.color.R
+		G = self.G * x.color.G
+		B = self.B * x.color.B
+	else
+		return error("Color input is not valid!")
+	end
+	return colorE.new(R,G,B)
+end
+
+function colorE:div(x:Color3 | Vector3 | ColorE)
 	local R,G,B
 	local t = typeof(x)
 	if t == "Color3" then
@@ -337,7 +369,32 @@ function colorE:div(x:Color3 | Vector3 | 'ColorE')
 	return colorE.new(R,G,B)
 end
 
-function colorE:add(x:number | Color3 | Vector3 | 'ColorE')
+function colorE:__div(x:Color3 | Vector3 | ColorE)
+	local R,G,B
+	local t = typeof(x)
+	if t == "Color3" then
+		R = self.R / x.R
+		G = self.G / x.G
+		B = self.B / x.B
+	elseif t == "Vector3" then
+		R = self.R / x.X
+		G = self.G / x.Y
+		B = self.B / x.Z
+	elseif t == "number" then
+		R = self.R / x
+		G = self.G / x
+		B = self.B / x
+	elseif t == "table" then
+		R = self.R / x.color.R
+		G = self.G / x.color.G
+		B = self.B / x.color.B
+	else
+		return error("Color input is not valid!")
+	end
+	return colorE.new(R,G,B)
+end
+
+function colorE:add(x:number | Color3 | Vector3 | ColorE)
 	local R,G,B
 	local t = typeof(x)
 	if t == "Color3" then
@@ -362,7 +419,32 @@ function colorE:add(x:number | Color3 | Vector3 | 'ColorE')
 	return colorE.new(R,G,B)
 end
 
-function colorE:sub(x:number | Color3 | Vector3 | 'ColorE')
+function colorE:__add(x:number | Color3 | Vector3 | ColorE)
+	local R,G,B
+	local t = typeof(x)
+	if t == "Color3" then
+		R = self.R + x.R
+		G = self.G + x.G
+		B = self.B + x.B
+	elseif t == "Vector3" then
+		R = self.R + x.X
+		G = self.G + x.Y
+		B = self.B + x.Z
+	elseif t == "number" then
+		R = self.R + x
+		G = self.G + x
+		B = self.B + x
+	elseif t == "table" then
+		R = self.R + x.color.R
+		G = self.G + x.color.G
+		B = self.B + x.color.B
+	else
+		return error("Color input is not valid!")
+	end
+	return colorE.new(R,G,B)
+end
+
+function colorE:sub(x:number | Color3 | Vector3 | ColorE)
 	local R,G,B
 	local t = typeof(x)
 	if t == "Color3" then
@@ -387,7 +469,32 @@ function colorE:sub(x:number | Color3 | Vector3 | 'ColorE')
 	return colorE.new(R,G,B)
 end
 
-function colorE:invert(x:number | Color3 | Vector3 | 'ColorE')
+function colorE:__sub(x:number | Color3 | Vector3 | ColorE)
+	local R,G,B
+	local t = typeof(x)
+	if t == "Color3" then
+		R = self.R - x.R
+		G = self.G - x.G
+		B = self.B - x.B
+	elseif t == "Vector3" then
+		R = self.R - x.X
+		G = self.G - x.Y
+		B = self.B - x.Z
+	elseif t == "number" then
+		R = self.R - x
+		G = self.G - x
+		B = self.B - x
+	elseif t == "table" then
+		R = self.R - x.color.R
+		G = self.G - x.color.G
+		B = self.B - x.color.B
+	else
+		return error("Color input is not valid!")
+	end
+	return colorE.new(R,G,B)
+end
+
+function colorE:invert(x)
 	local R,G,B
 	local t = typeof(x)
 	if t == "Color3" then
@@ -408,7 +515,28 @@ function colorE:invert(x:number | Color3 | Vector3 | 'ColorE')
 	return colorE.new(R,G,B)
 end
 
-function colorE:min(x:Color3 | Vector3 | 'ColorE',mn:number)
+function colorE:__unm(x:number | Color3 | Vector3 | ColorE)
+	local R,G,B
+	local t = typeof(x)
+	if t == "Color3" then
+		R = 1 - x.R
+		G = 1 - x.G
+		B = 1 - x.B
+	elseif t == "Vector3" then
+		R = 1 - x.X
+		G = 1 - x.Y
+		B = 1 - x.Z
+	elseif t == "table" then
+		R = 1 - x.R
+		G = 1 - x.G
+		B = 1 - x.B
+	else
+		return error("Color input is not valid!")
+	end
+	return colorE.new(R,G,B)
+end
+
+function colorE:min(x,mn)
 	local R,G,B
 	local t = typeof(x)
 	if t == "Color3" then
@@ -429,7 +557,7 @@ function colorE:min(x:Color3 | Vector3 | 'ColorE',mn:number)
 	return colorE.new(R,G,B)
 end
 
-function colorE:max(x:Color3 | Vector3 | 'ColorE',mx:number)
+function colorE:max(x,mx)
 	local R,G,B
 	local t = typeof(x)
 	if t == "Color3" then
@@ -450,7 +578,7 @@ function colorE:max(x:Color3 | Vector3 | 'ColorE',mx:number)
 	return colorE.new(R,G,B)
 end
 
-function colorE:range(x:Color3 | Vector3 | 'ColorE',mn:number,mx:number)
+function colorE:range(x,mn,mx)
 	return colorE:max(colorE:min(x,mn),mx)
 end
 
